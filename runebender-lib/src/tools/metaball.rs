25aa6309a109eace6a39119518a0d17534c62c7e
Eli Heuer <elih@protonmail.com>
2023-01-03 12:41:20 -0800

    Add clean slate Metaball.rs starting point

--- runebender-lib/src/tools/metaball.rs
+++ runebender-lib/src/tools/metaball.rs
@@ -1,12 +1,6 @@
 //! The metaball tool
 
-use druid::{Env, EventCtx, KbKey, KeyEvent, MouseEvent};
-
-use crate::design_space::DPoint;
-use crate::edit_session::EditSession;
-use crate::mouse::{Drag, Mouse, MouseDelegate, TaggedEvent};
-use crate::point::{EntityId, PathPoint};
-use crate::tools::{EditType, Tool, ToolId};
+use crate::tools::{Tool, ToolId};
 
 /// The state of the Metaball tool.
 #[derive(Debug, Default, Clone, PartialEq)]
@@ -17,11 +11,9 @@ pub struct Metaball {
 impl Metaball {
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 enum State {
     Ready,
-    /// The mouse is down and has added a new point.
-    AddPoint(EntityId),
 }
 
 impl Tool for Metaball {
